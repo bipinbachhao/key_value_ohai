cf5a6140c0d74e5b3e7fc8a961c265087e8eabfa
bipinbachhao <bipinbachhao@gmail.com>
2017-02-14 23:23:15 -0500

    Added ohai dependencies for chef spec ohai plugin, update metadata file

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
 name 'key_value_ohai'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures key_value_ohai'
+maintainer 'Bipin Bachhao'
+maintainer_email 'bipinbachhao@gmail.com'
+license 'apache2'
+description 'Installs/Configures key_value_ohai plugin'
 long_description 'Installs/Configures key_value_ohai'
 version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/key_value_ohai/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/bipinbachhao/key_value_ohai/issues' if respond_to?(:issues_url)
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/key_value_ohai' if respond_to?(:source_url)
+source_url 'https://github.com/bipinbachhao/key_value_ohai' if respond_to?(:source_url)
